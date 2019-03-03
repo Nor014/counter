d9b3fd9977c841202b91287b51fadca7bb7eba6d
Nor014 <metal_14@mail.ru>
2019-03-03 11:19:40 +0300

    fix

--- js/counter.js
+++ js/counter.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', onLoad);
 
 function onLoad() {
   counter.innerHTML = localStorage.counter ? localStorage.counter : 0;
+  localStorage.setItem('counter', counter.innerHTML);
 }
 
 let buttons = document.querySelector(".wrap-btns");
@@ -11,9 +12,16 @@ let counter = document.getElementById('counter');
 
 buttons.addEventListener('click', onClick);
 
+// function onClick() {
+//   if (event.target.id === 'increment') counter.innerHTML++
+//   if (event.target.id === 'decrement' && counter.innerHTML > 0) counter.innerHTML--
+//   if (event.target.id === 'reset') counter.innerHTML = 0;
+//   localStorage.setItem('counter', counter.innerHTML);
+// }
+
 function onClick() {
-  if (event.target.id === 'increment') counter.innerHTML++
-  if (event.target.id === 'decrement' && counter.innerHTML > 0) counter.innerHTML--
-  if (event.target.id === 'reset') counter.innerHTML = 0;
-  localStorage.setItem('counter', counter.innerHTML);
+  if (event.target.id === 'increment') localStorage.counter++;
+  if (event.target.id === 'decrement' && localStorage.counter > 0) localStorage.counter--;
+  if (event.target.id === 'reset') localStorage.counter = 0;
+  counter.innerText = localStorage.counter;
 }
